d1bd5960893d7ea224a330f01391c555036932b8
rickard <rickard@montania.se>
2014-02-02 19:58:57 +0100

    Smalare artiklar, lite marginaler, bort med dubbeltaggar, entry-meta och footer mindre text

--- entry-meta.php
+++ entry-meta.php
@@ -2,7 +2,5 @@
     Av <?php the_author() ?>, publicerad i <?php the_category(', ', '') ?>
     <time class="published updated" pubdate datetime="<?php echo date( "Y-m-d", get_the_date( 'U' ) ) ?>"><?php echo relative_time_sv( get_the_date( 'U' ) ); ?></time>.
 
-    <?php if (get_the_tag_list()) : ?>
-        Taggad <?php the_tags() ?>
-    <?php endif ?>
+     <?php the_tags('Taggad: ') ?>
 </span>
